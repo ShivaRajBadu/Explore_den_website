4187141f800e22c1c2a3284aabc4c336a1affcdc
shiva raj badu <shivabadu5@gmail.com>
2024-06-24 10:42:40 +0545

    spelling change

--- src/components/main_page/ContactUs.tsx
+++ src/components/main_page/ContactUs.tsx
@@ -8,7 +8,7 @@ const ContactUs = () => {
       <div className="max-w-[1440px] w-full md:w-[90%] mx-auto">
         <div className="bg-brand text-background px-4  md:rounded-[40px] text-center py-[72px]">
           <h1 className="font-semibold text-[28px] md:text-5xl capitalize py-4">
-            Have any Qureies ? Contact Us
+            Have any Queries? Contact Us
           </h1>
           <p className="font-normal  text-sm md:text-lg py-2 max-w-[70%] md:max-w-[50%] text-balance mb-10 mx-auto">
             Our team is here to help you find the best travel experiences. Get
