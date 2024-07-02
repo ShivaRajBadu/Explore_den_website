ab81d7c8df1a8443f5caa2701fc9a3cc10d3152b
shiva raj badu <shivabadu5@gmail.com>
2024-07-02 19:42:36 +0545

    error page changes

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -12,6 +12,7 @@ export default function GlobalError({
       <body>
         <h2>Something went wrong!</h2>
         <p>{error.message}</p>
+        <p>{error.digest}</p>
         <button onClick={() => reset()}>Try again</button>
       </body>
     </html>
